9616f3499e5cefeeb8ef00cd9e3487da5ab7a3da
Tatum Alenko <tatum.alenko@gmail.com>
2019-03-15 02:42:20 -0400

    [#41] Fix username validation and simplify control flow

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const passwordValidator = require('password-validator');
+const PasswordValidator = require('password-validator');
 const { User } = require('../database/models/User');
 const passport = require('../passport');
-
-// const { User } = require('../database/models/User');
-// const passport = require('../passport/index');
-
 
 router.post('/', async (req, res) => {
   try {
@@ -16,22 +12,40 @@ router.post('/', async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
 
-    const passwordRules = new passwordValidator();
-    console.log(passwordRules);
+    const passwordRules = new PasswordValidator();
+    const usernameRules = new PasswordValidator();
 
-    // password rules
+    // Username rules
+    usernameRules
+      .is().min(8) // Minimum length 8
+      .is().max(100) // Maximum length 100
+      .has().not().spaces(); // Should not have spaces
+
+    // Password rules
     passwordRules
-      .is().min(6) // minimum length of 6
-      .is().max(20) // maximum length of 20
-      .has()
-      .not()
-      .spaces() // no spaces allowed
-      .has()
-      .lowercase() // must have lowercase letters
-      .is()
-      .not()
-      .oneOf([ username, 'password', 'abcdef', '123456' ]); // list of invalid passwords
+      .is().min(8) // Minimum length 8
+      .is().max(100) // Maximum length 100
+      .has().not().spaces() // Should not have spaces
+      .has().uppercase() // Must have uppercase letters
+      .has().lowercase() // Must have lowercase letters
+      .has().digits() // Must have digits
+      .is().not().oneOf([ 'Passw0rd', 'Password123' ]); // Blacklist these values
 
+    const usernameValidatorMessageMap = {
+      min: 'Username must be 6 characters or more.',
+      max: 'Username must be 100 characters or less.',
+      spaces: 'Username cannot contain spaces.',
+    };
+
+    const passwordValidatorMessageMap = {
+      min: 'Password must be 6 characters or more.',
+      max: 'Password must be 100 characters or less.',
+      spaces: 'Password cannot contain spaces.',
+      uppercase: 'Password must contain uppercase letters.',
+      lowercase: 'Password must contain lowercase letters.',
+      digits: 'Password must contain at least 1 digit.',
+      oneOf: 'Password is too simple.',
+    };
 
     if (user) { // user already exists
       res
@@ -40,7 +54,30 @@ router.post('/', async (req, res) => {
           message: `Sorry, a user already exists with the username: ${username}`,
           user: null,
         });
-    } else if (passwordRules.validate(password)) { // valid password
+    } else if (username === password) {
+      res
+        .status(400)
+        .json({
+          message: 'Username and password cannot be the same.',
+          user: null,
+        });
+    } else if (!usernameRules.validate(username)) {
+      const reasons = usernameRules.validate(username, { list: true });
+      res
+        .status(400)
+        .json({
+          message: usernameValidatorMessageMap[reasons[0]],
+          user: null,
+        });
+    } else if (!passwordRules.validate(password)) {
+      const reasons = passwordRules.validate(password, { list: true });
+      res
+        .status(400)
+        .json({
+          message: passwordValidatorMessageMap[reasons[0]],
+          user: null,
+        });
+    } else {
       const newUser = new User({
         username,
         password,
@@ -52,42 +89,6 @@ router.post('/', async (req, res) => {
           message: 'OK',
           user: savedUser,
         });
-    } else { // password does not respect the rules
-      if (username === password) {
-        res
-          .status(400)
-          .json({
-            message: 'Username and password cannot be the same.',
-          });
-      }
-      if (username.is().oneOf([ 'password', 'abcdef', '123456' ])) { // password too simple
-        res
-          .status(400)
-          .json({
-            message: 'Password is too simple.',
-          });
-      }
-      if (username.has().not().lowercase()) { // password does not have any lowercase letters
-        res
-          .status(400)
-          .json({
-            message: 'Password must contain lowercase letters.',
-          });
-      }
-      if (password.length < 6) { // password too short
-        res
-          .status(400)
-          .json({
-            message: 'Password must be 6 characters or more.',
-          });
-      }
-      if (password.has().spaces()) { // password contains spaces
-        res
-          .status(400)
-          .json({
-            message: 'Password cannot contain spaces.',
-          });
-      }
     }
   } catch (err) {
     res
