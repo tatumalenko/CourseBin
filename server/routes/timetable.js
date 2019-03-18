2c794d2a63ca1725ebd727e08fabd1a4cf893de1
Jay Chen <sinnohboi@gmail.com>
2019-03-18 16:30:32 -0400

    [#85] Create sample course objects and send an array of them to front end

--- server/routes/timetable.js
+++ server/routes/timetable.js
@@ -5,8 +5,53 @@ const { Timetable } = require('../database/models/Timetable');
 
 router.get('/', async (request, response) => {
   try {
-    const timetable = await Timetable.find({});
-    if (timetable) {
+    //const timetable = await Timetable.find({}); // change this back after
+    const timetable = [
+      {
+        title: 'COMP-346, LECTURE UU',
+        // Year, month, day, time (9:30AM)
+        startDate: new Date(2018, 6, 25, 9, 30),
+        endDate: new Date(2018, 6, 25, 11, 30),
+        id: 0,
+    
+      },
+      {
+        title: 'BIOL-206 LECTURE',
+        startDate: new Date(2018, 6, 24, 12, 0),
+        endDate: new Date(2018, 6, 24, 13, 0),
+        id: 1,
+      },
+      {
+        title: 'ELEC-275 LECTURE',
+        startDate: new Date(2018, 6, 23, 8, 30),
+        endDate: new Date(2018, 6, 23, 10, 15),
+        id: 3,
+      },
+      {
+        title: 'SOEN-341 LECTURE',
+        startDate: new Date(2018, 6, 26, 8, 30),
+        endDate: new Date(2018, 6, 26, 10, 15),
+        id: 4,
+      },
+      {
+        title: 'SOEN-331 LECTURE',
+        startDate: new Date(2018, 6, 27, 11, 30),
+        endDate: new Date(2018, 6, 27, 13, 0),
+        id: 5,
+      },
+    ];
+    // ===== SAMPLE COURSE FORMAT =====
+    /*
+    {
+      title: 'COMP-346, LECTURE SS',
+      // Year, month, day, time (9:30AM)
+      startDate: new Date(2018, 6, 25, 9, 30),
+      endDate: new Date(2018, 6, 25, 11, 30),
+      id: 0,
+  
+    }
+    */
+    if (true) { // change back to if (timetable) after
       response
         .status(200)
         .json({
