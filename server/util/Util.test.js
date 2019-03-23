563cde648df11b34869818e90de47cfce30e26bb
Tatum Alenko <tatum.alenko@gmail.com>
2019-03-23 04:54:32 -0400

    [#31] Add subCombinations test

--- server/util/Util.test.js
+++ server/util/Util.test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 
 const { Util } = require('./Util');
 
-test('Integers - all non-empty arrays', (t) => {
+test('allCombinations: Integers - all non-empty arrays', (t) => {
   t.deepEqual(Util.allCombinations([
     [ 1, 2, 3 ],
     [ 4 ],
@@ -16,7 +16,7 @@ test('Integers - all non-empty arrays', (t) => {
     [ 3, 4, 6 ] ]);
 });
 
-test('Integers - 1 empty array', (t) => {
+test('allCombinations: Integers - 1 empty array', (t) => {
   t.deepEqual(Util.allCombinations([
     [ 1, 2, 3 ],
     [ 4 ],
@@ -25,6 +25,21 @@ test('Integers - 1 empty array', (t) => {
     [ 1, 4 ],
     [ 2, 4 ],
     [ 3, 4 ] ]);
+});
+
+test('subCombinations: Integers - 3 of 5', (t) => {
+  t.deepEqual(Util.subCombinations([ 1, 2, 3, 4, 5 ], 3), [
+    [ 1, 2, 3 ],
+    [ 1, 2, 4 ],
+    [ 1, 2, 5 ],
+    [ 1, 3, 4 ],
+    [ 1, 3, 5 ],
+    [ 1, 4, 5 ],
+    [ 2, 3, 4 ],
+    [ 2, 3, 5 ],
+    [ 2, 4, 5 ],
+    [ 3, 4, 5 ],
+  ]);
 });
 
 // const arr = [ [ 1, 2, 3 ], [ 4 ], [ 5, 6 ] ];
