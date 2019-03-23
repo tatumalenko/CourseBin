10c875c730ea54e660d1ffba5c2038f98c5e17de
Tatum Alenko <tatum.alenko@gmail.com>
2019-03-23 04:55:53 -0400

    [#31] Add some functionality to findCandidateSchedules

--- server/database/models/ProgramBuilder.js
+++ server/database/models/ProgramBuilder.js
@@ -3,7 +3,9 @@ const _ = require('lodash');
 
 const { Catalog } = require('./Catalog');
 const { Timetable } = require('./Timetable');
-// const { Util } = require('./../../util/Util');
+const { Schedule } = require('./Schedule');
+const { Sequence } = require('./Sequence');
+const { Util } = require('./../../util/Util');
 
 class ProgramBuilder {
   static findUncompletedCourses({ requiredCourses, completedCourses }) {
@@ -95,11 +97,115 @@ class ProgramBuilder {
     return categorizedSectionQueue;
   }
 
-  static async findCandidateSchedules({ completedCourses, requiredCourses, term }) {
-    return this.findCandidateSectionQueueMap({ completedCourses, requiredCourses, term });
+  static async findCandidateTermSchedules({ completedCourses, requiredCourses, termPreference }) {
+    // TODO: trying to find all possible combinations of non-specified courses
+    // (i.e. of all candidates courses) might be too many possibilities and might
+    // not only make the options too numerous too display but severely hamper the
+    // speed. To this end, perhaps the best way to proceed is to force the user
+    // to select all courses they wish to register for each term and us not try
+    // to propose a million options.
+    const sectionQueueMap = this.findCandidateSectionQueueMap({
+      completedCourses,
+      requiredCourses,
+      term: termPreference.term,
+    });
+
+    // Convert map to primitive object because I can't deal with remembering what
+    // is an primitive object and what is a Map object
+    const courseCodeSectionArrayMap = Util.objectFromMap(sectionQueueMap);
+
+    // For each key in object (course code), categorize the array of sections
+    // into subobject of categorized LEC/TUT/LAB components
+    const categorizedCourseCodeSectionArrayMap = {};
+    Object.keys(courseCodeSectionArrayMap)
+      .forEach((key) => {
+        categorizedCourseCodeSectionArrayMap[key] = this.categorizeSectionQueueIntoKind(courseCodeSectionArrayMap[key]);
+      });
+
+    // For each key in object (course code), store the set of all combinations
+    // for the section's components
+    const courseCodeSectionCombinationArrayMap = {};
+    Object.keys(categorizedCourseCodeSectionArrayMap)
+      .forEach((key) => {
+        courseCodeSectionCombinationArrayMap[key] = Util.allCombinations([
+          categorizedCourseCodeSectionArrayMap[key].LEC,
+          categorizedCourseCodeSectionArrayMap[key].TUT,
+          categorizedCourseCodeSectionArrayMap[key].LAB,
+        ]);
+      });
+
+    // Convert map into an array of arrays, where each array is the combinations
+    // determined in courseCodeSectionCombinationArrayMap
+    const sectionCombinationJaggedArray =
+      Object.keys(courseCodeSectionCombinationArrayMap)
+        .map(key => courseCodeSectionCombinationArrayMap[key]);
+
+    // Find the kCn sets first so that we don't try to find all combinations
+    // of like 10 sections
+    const sectionSubCombinations = Util.subCombinations(
+      sectionCombinationJaggedArray,
+      termPreference.numberOfCourses,
+    );
+
+    // Find all the combinations of each subSectionCombination of kCn sets
+    // This will become a triple nested object (array of combos, where each
+    // combo is a jagged array)
+    const sectionCombinations = sectionSubCombinations.map(e => Util.allCombinations(e));
+
+    // Flatten the results from sectionCombinations because we only care about
+    // all the kCn sets of combinations, we don't care about which combinations
+    // come from which subset of sets?! This is really confusing...
+    const flatSectionCombinations = _.flatten(sectionCombinations);
+
+    // Find all possible combinations of up to termPreference.numberOfCourses section
+    // groups (LEC+TUT+LAB) of those hopefully including the ones the student
+    // requested (pick out of those)
+
+    // Start by filtering the sectionQueueMap entries by those student requested
+    // const filteredSectionQueueMap =
+    //   Object.keys(sectionQueueMap)
+    //     .filter(
+    //       key => termPreference.requiredCourses.includes(sectionQueueMap[key]),
+    //     )
+    //     .map(key => sectionQueueMap[key]);
+
+    // if (Object.keys(filteredSectionQueueMap).length < termPreference.numberOfCourses) {
+    //   const leftoverSectionQueueMapKeys = _.difference(
+    //     Object.keys(sectionQueueMap),
+    //     Object.keys(filteredSectionQueueMap),
+    //   );
+
+    //   const leftoverSectionQueueMap = leftoverSectionQueueMapKeys.map(key => sectionQueueMap[key]);
+
+    //   const leftoverCoursesToAdd = Object.keys(filteredSectionQueueMap).length - termPreference.numberOfCourses;
+
+    //   // Add the other section queues to the filtered map
+    //   // TODO: Should filter further among these by offDays & timeOfDay preference
+    //   filteredSectionQueueMap.push(...leftoverSectionQueueMap.slice(0, leftoverCoursesToAdd));
+    // }
+
+    const schedule = new Schedule({ term: termPreference.term, sections: flatSectionCombinations });
+
+    return schedule;
   }
 
-  static async findCandidateSequences({ completedCourses, requiredCourses, termPreferences }) {
+  static async findCandidateSchedules({ completedCourses, requiredCourses, preferences }) {
+    const fallSchedules = this.findCandidateTermSchedules({ completedCourses, requiredCourses, termPreference: preferences.fall });
+    const winterSchedules = this.findCandidateTermSchedules({ completedCourses, requiredCourses, termPreference: preferences.winter });
+    const summerSchedules = this.findCandidateTermSchedules({ completedCourses, requiredCourses, termPreference: preferences.summer });
+
+    return {
+      fall: fallSchedules,
+      winter: winterSchedules,
+      summer: summerSchedules,
+    };
+  }
+
+  static async findCandidateSequences({
+    completedCourses,
+    requiredCourses,
+    Preferences,
+  }) {
     let completed = completedCourses;
     const required = requiredCourses;
     const termCourses = [];
@@ -110,14 +216,14 @@ class ProgramBuilder {
     // Get all candidate courses given currently completed courses
     let candidateCourses = await this.findCandidateCourses({ completedCourses: completed, requiredCourses: required });
     while (_.difference(required, completed).length > 0) {
-      if (termPreferences[terms[termTracker % numberOfTerms]].numberOfCourses !== 0) {
-        // Of the candidate courses, pick at most 5, and add those to the lot of completedCOurses
-        const thisTerm = {
+      if (Preferences[terms[termTracker % numberOfTerms]].numberOfCourses !== 0) {
+        // Of the candidate courses, pick at most the number specified in termPreferences, and add those to the lot of completedCOurses
+        const sequence = new Sequence({
           term: terms[termTracker % numberOfTerms],
-          courses: candidateCourses.slice(0, termPreferences[terms[termTracker % numberOfTerms]].numberOfCourses),
-        };
-        termCourses.push(thisTerm);
-        completed = _.uniq([ ...completed, ..._.flatten(thisTerm.courses) ]);
+          courses: candidateCourses.slice(0, Preferences[terms[termTracker % numberOfTerms]].numberOfCourses),
+        });
+        termCourses.push(sequence);
+        completed = _.uniq([ ...completed, ..._.flatten(sequence.courses) ]);
         // eslint-disable-next-line
         candidateCourses = await this.findCandidateCourses({ completedCourses: completed, requiredCourses: required });
       }
@@ -127,428 +233,15 @@ class ProgramBuilder {
     return termCourses;
   }
 
-  static async getCandidateSchedulesAndSequences({
+  static async getCandidatePlans({
     completedCourses,
     requiredCourses,
-    termPreferences,
+    preferences,
   }) {
-    const candidatesSchedules = this.findCandidateSchedules({ completedCourses, requiredCourses });
+    const candidatesSchedules = this.findCandidateSchedules({ completedCourses, requiredCourses, preferences });
     // The completedCourses would need to be updated with those included in schedules
-    const candidateSequences = this.findCandidateSequences({ completedCourses, requiredCourses });
+    const candidateSequences = this.findCandidateSequences({ completedCourses, requiredCourses, preferences });
   }
 }
 
 module.exports = { ProgramBuilder };
-
-// const hashQueueMap = {
-//   SOEN331: [ {
-//     _id: '5c7416be14d29885ffb38b96',
-//     courseCode: 'SOEN331',
-//     code: 'U',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38b9c',
-//     courseCode: 'SOEN331',
-//     code: 'U UA',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38ba2',
-//     courseCode: 'SOEN331',
-//     code: 'U UB',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38be1',
-//     courseCode: 'SOEN331',
-//     code: 'W',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38be7',
-//     courseCode: 'SOEN331',
-//     code: 'W WA',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38bea',
-//     courseCode: 'SOEN331',
-//     code: 'W WB',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38cb2',
-//     courseCode: 'SOEN331',
-//     code: 'U UC',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38cb8',
-//     courseCode: 'SOEN331',
-//     code: 'U UD',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38cbb',
-//     courseCode: 'SOEN331',
-//     code: 'U UE',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38d37',
-//     courseCode: 'SOEN331',
-//     code: 'W WC',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38d3d',
-//     courseCode: 'SOEN331',
-//     code: 'W WD',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38d43',
-//     courseCode: 'SOEN331',
-//     code: 'W WE',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38d46',
-//     courseCode: 'SOEN331',
-//     code: 'W WF',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38d49',
-//     courseCode: 'SOEN331',
-//     code: 'W WG',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   } ],
-//   COMP335: [ {
-//     _id: '5c7416be14d29885ffb3876b',
-//     courseCode: 'COMP335',
-//     code: 'N',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38775',
-//     courseCode: 'COMP335',
-//     code: 'N NA',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb3886f',
-//     courseCode: 'COMP335',
-//     code: 'N NB',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [],
-//     __v: 0,
-//   } ],
-//   COMP346: [ {
-//     _id: '5c7416be14d29885ffb3877e',
-//     courseCode: 'COMP346',
-//     code: 'NI-X',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LAB',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38781',
-//     courseCode: 'COMP346',
-//     code: 'NJ-X',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LAB',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38795',
-//     courseCode: 'COMP346',
-//     code: 'NN',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb3879e',
-//     courseCode: 'COMP346',
-//     code: 'NNNA',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb387ac',
-//     courseCode: 'COMP346',
-//     code: 'NNNB',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38809',
-//     courseCode: 'COMP346',
-//     code: 'WI-X',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LAB',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb3880c',
-//     courseCode: 'COMP346',
-//     code: 'WJ-X',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LAB',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38811',
-//     courseCode: 'COMP346',
-//     code: 'WW',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38814',
-//     courseCode: 'COMP346',
-//     code: 'WWWA',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38817',
-//     courseCode: 'COMP346',
-//     code: 'WWWB',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb388de',
-//     courseCode: 'COMP346',
-//     code: 'YY',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb389ad',
-//     courseCode: 'COMP346',
-//     code: 'UU',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38aae',
-//     courseCode: 'COMP346',
-//     code: 'UUUA',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38ab1',
-//     courseCode: 'COMP346',
-//     code: 'UUUB',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38ad8',
-//     courseCode: 'COMP346',
-//     code: 'YYYA',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38adb',
-//     courseCode: 'COMP346',
-//     code: 'YYYB',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   } ],
-//   COMP348: [ {
-//     _id: '5c7416be14d29885ffb38767',
-//     courseCode: 'COMP348',
-//     code: 'E',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'LEC',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb3896b',
-//     courseCode: 'COMP348',
-//     code: 'E EI',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb3896e',
-//     courseCode: 'COMP348',
-//     code: 'E EJ',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   {
-//     _id: '5c7416be14d29885ffb38971',
-//     courseCode: 'COMP348',
-//     code: 'E EK',
-//     term: 'FALL',
-//     mode: 'In Person',
-//     location: [ Object ],
-//     kind: 'TUT',
-//     times: [ Array ],
-//     __v: 0,
-//   },
-//   ],
-// };
-
-// console.log(ProgramBuilder.categorizeSectionQueueIntoKind(hashQueueMap.SOEN331));
