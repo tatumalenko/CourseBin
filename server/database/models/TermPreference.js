93fd7218420c822ec9400e04ab3ef716a08b28e0
Tatum Alenko <tatum.alenko@gmail.com>
2019-03-26 03:22:28 -0400

    [#31] Add term attribute

--- server/database/models/TermPreference.js
+++ server/database/models/TermPreference.js
@@ -1,11 +1,17 @@
 class TermPreference {
   constructor({
+    term = null,
     numberOfCourses = 5,
     requestedCourses = null,
     eveningTimePreference = false,
     timeOfDay = null,
     offDays = null,
   }) {
+    if (!term) {
+      throw new Error('TermPreference.constructor: Must specify term!');
+    }
+
+    this.term = term;
     this.numberOfCourses = numberOfCourses;
     this.requestedCourses = requestedCourses;
     this.eveningTimePreference = eveningTimePreference;
