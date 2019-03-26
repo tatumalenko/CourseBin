954d48067d2c26b689521a6ade2f5accd6905697
Tatum Alenko <tatum.alenko@gmail.com>
2019-03-26 01:30:22 -0400

    [#31] Add Section class

---
+++ server/database/models/Section.js
@@ -0,0 +1,22 @@
+const { Location } = require('./Location');
+const { TimeBlock } = require('./TimeBlock');
+
+class Section {
+  constructor({
+    courseCode,
+    code,
+    kind,
+    location,
+    mode,
+    times,
+  }) {
+    this.courseCode = courseCode;
+    this.code = code;
+    this.kind = kind;
+    this.mode = mode;
+    this.location = new Location(location);
+    this.times = [ ...times ].map(e => new TimeBlock(e)); // Needed to convert to primitive Array
+  }
+}
+
+module.exports = { Section };
