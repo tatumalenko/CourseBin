f677b92e5db3487beeaea573fb24c81f4634d757
Tatum Alenko <tatum.alenko@gmail.com>
2019-03-26 03:24:37 -0400

    [#31] Add empty sortSchedules method body

--- server/database/models/Plan.js
+++ server/database/models/Plan.js
@@ -3,6 +3,14 @@ class Plan {
     this.schedules = schedules;
     this.sequences = sequences;
   }
+
+  static sortSchedules({ schedules, termPreference }) {
+    // Sorts the array of Schedule object in place such that the front elements
+    // represent a higher 'priority' in accordance to the TermPreference argument.
+    // In other words, the Schedule element at index 0 should represent the
+    // schedule that most closely matches the preferences passed (e.g. such
+    // as the eveningTimePreference option, etc).
+  }
 }
 
 module.exports = { Plan };
