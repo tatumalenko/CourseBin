6c134de5818fe02ac32a980155cf704d77843758
leojrsilao <leojrsilao@gmail.com>
2019-03-29 12:05:11 -0400

    [#92] Add detailed boxes of schedule

---
+++ client/src/components/box-child.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+
+const childBox = (props) => {
+  
+ 
+    return (
+      <div className="box-container"> 
+        <Grid container>
+          <Grid item xs={12}>
+            <Grid container >
+              <Card className="card-container">
+                <CardContent> 
+                
+                   <Typography color="inherit">
+                      {props.titleClass}
+                    </Typography>
+                    <Typography color="inherit">
+                      {props.subject}
+                    </Typography>
+                    
+                    <Typography color="inherit">
+                    {props.semester}
+                    </Typography>
+
+                    <Typography color="inherit">
+                    {props.lecture}
+                    </Typography>
+                   
+                    <Typography color="inherit">
+                    {props.tutorial}
+                    </Typography>
+                   
+                  
+                </CardContent>
+              
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+  
+      </div>
+    )
+}
+
+export default childBox;
