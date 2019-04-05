f9328fff6c25901c9fdb9df25084a21ded5423e2
Tatum Alenko <tatum.alenko@gmail.com>
2019-04-05 07:50:51 -0400

    [#106] Add sortSchedules implementation and tests

--- server/database/models/Plan.js
+++ server/database/models/Plan.js
@@ -1,28 +1,84 @@
-const { Preferences } = require('./Preferences');
-const { TermPreference } = require('./TermPreference');
-
-/*
-
-We need to take the following into consideration when ranking schedules:
-
-- day or evening
-- number of courses
-- requested courses?
-
-*/
+const _ = require('lodash');
 
 class Plan {
   constructor({ schedules, sequences }) {
-    this.schedules = schedules;
+    this.schedules = {
+      fall: schedules.fall,
+      winter: schedules.winter,
+      summer: schedules.summer,
+    };
     this.sequences = sequences;
   }
 
-  static sortSchedules({ schedules, termPreference }) {
+  sortSchedules({ preferences }) {
     // Sorts the array of Schedule object in place such that the front elements
     // represent a higher 'priority' in accordance to the TermPreference argument.
     // In other words, the Schedule element at index 0 should represent the
     // schedule that most closely matches the preferences passed (e.g. such
     // as the eveningTimePreference option, etc).
+    const weightMap = {
+      '08.00.00': {
+        early: 12,
+        late: -2,
+      },
+      '10.00.00': {
+        early: 10,
+        late: 2,
+      },
+      '12.00.00': {
+        early: 8,
+        late: 4,
+      },
+      '14.00.00': {
+        early: 6,
+        late: 6,
+      },
+      '16.00.00': {
+        early: 4,
+        late: 8,
+      },
+      '18.00.00': {
+        early: 2,
+        late: 10,
+      },
+      '20.00.00': {
+        early: -2,
+        late: 12,
+      },
+    };
+
+    const scheduleWeight = (schedule, termPreference) => {
+      let earlyScore = 0;
+      let lateScore = 0;
+
+      const scheduleTimeBlocks = _.flatten(
+        schedule.sections.map(section => section.times),
+      );
+
+      scheduleTimeBlocks.forEach((timeBlock) => {
+        Object.keys(weightMap).forEach((timeKey, timeIdx) => {
+          if (timeBlock.startTime >= timeKey &&
+            (Object.keys(weightMap).length - 1 >= timeIdx + 1 ?
+              timeBlock.startTime < Object.keys(weightMap)[timeIdx + 1] :
+              true)) {
+            earlyScore += weightMap[timeKey].early;
+            lateScore += weightMap[timeKey].late;
+          }
+        });
+      });
+
+      return termPreference.eveningTimePreference ? lateScore : earlyScore;
+    };
+
+    this.schedules.fall.sort(
+      (s1, s2) => scheduleWeight(s2, preferences.fall) - scheduleWeight(s1, preferences.fall),
+    );
+    this.schedules.winter.sort(
+      (s1, s2) => scheduleWeight(s2, preferences.winter) - scheduleWeight(s1, preferences.winter),
+    );
+    this.schedules.summer.sort(
+      (s1, s2) => scheduleWeight(s2, preferences.summer) - scheduleWeight(s1, preferences.summer),
+    );
   }
 }
 
--- server/database/models/Plan.test.js
+++ server/database/models/Plan.test.js
@@ -1,89 +1,242 @@
 const test = require('ava');
 
 const { Plan } = require('./Plan');
+const { Preferences } = require('./Preferences');
 const { TermPreference } = require('./TermPreference');
 const { Schedule } = require('./Schedule');
 const { Section } = require('./Section');
 const { Location } = require('./Location');
 const { TimeBlock } = require('./TimeBlock');
 
-test('sortSchedule 1 - Empty termPreference object', (t) => {
-  // TODO: Implement first sortSchedule with empty termPreference passed in
-  // Should do nothing to the Schedule array passed in
-  t.pass();
+test('sortSchedules - Empty preferences object', (t) => {
+  const earlySchedule = createScheduleFromSections(
+    [
+      createSectionFromStartTimes([ '08.45.00' ], 'FALL'),
+    ],
+  );
+
+  const lateSchedule = createScheduleFromSections(
+    [
+      createSectionFromStartTimes([ '17.45.00' ], 'FALL'),
+    ],
+  );
+
+  const plan =
+    new Plan({
+      sequences: [],
+      schedules: {
+        fall: [ lateSchedule, earlySchedule ],
+        winter: [],
+        summer: [],
+      },
+    });
+
+  t.throws(() => plan.sortSchedules({ preferences: {} }),
+    TypeError,
+    'Cannot read property \'eveningTimePreference\' of undefined');
 });
 
-test('sortSchedule 2 - eveningTimePreference - 2 schedules with 1 section each', (t) => {
-  // TODO: Implement first sortSchedule with empty termPreference passed in
-  const earlySchedule = new Schedule({
-    term: 'FALL',
-    schedules: [
-      new Schedule({
-        term: 'FALL',
-        sections: [
-          new Section({
-            courseCode: 'COMP348',
-            code: 'H',
-            kind: 'LEC',
-            location: new Location({
-              code: 'H920',
-              building: 'Hall',
-              room: '920',
-            }),
-            mode: 'IN PERSON',
-            times: [
-              new TimeBlock({
-                startTime: '08:45:00',
-                endTime: '10:15:00',
-                weekDay: 'MONDAY',
-              }),
-            ],
-          }),
-        ],
-      }),
-    ],
+test('sortSchedules - Empty schedules', (t) => {
+  const plan =
+    new Plan({
+      sequences: [],
+      schedules: {
+        fall: [],
+        winter: [],
+        summer: [],
+      },
+    });
+
+  plan.sortSchedules({
+    preferences: createPreferencesFromEveningTimePreferences([ false, false, false ]),
   });
 
-  const lateSchedule = new Schedule({
-    term: 'FALL',
-    schedules: [
-      new Schedule({
-        term: 'FALL',
-        sections: [
-          new Section({
-            courseCode: 'COMP348',
-            code: 'H',
-            kind: 'LEC',
-            location: new Location({
-              code: 'H920',
-              building: 'Hall',
-              room: '920',
-            }),
-            mode: 'IN PERSON',
-            times: [
-              new TimeBlock({
-                startTime: '17:45:00',
-                endTime: '20:15:00',
-                weekDay: 'MONDAY',
-              }),
-            ],
-          }),
-        ],
-      }),
-    ],
-  });
+  t.deepEqual(plan, plan, 'Should be true since no schedules to sort');
+});
 
-  const termPreference = new TermPreference({
-    term: 'FALL',
-    numberOfCourses: 5,
-    requestedCourses: [],
-    eveningTimePreference: false,
-    timeOfDay: 'MORNING', // Optional (based on eveningTimePreference)
-    offDays: [ 'FRIDAY' ],
+test('sortSchedules - eveningTimePreference - 2 schedules with 1 section each', (t) => {
+  const earlySchedule = createScheduleFromSections(
+    [
+      createSectionFromStartTimes([ '08.45.00' ], 'FALL'),
+      createSectionFromStartTimes([ '17.45.00' ], 'FALL'),
+    ],
+  );
+
+  const lateSchedule = createScheduleFromSections(
+    [
+      createSectionFromStartTimes([ '17.45.00' ], 'FALL'),
+    ],
+  );
+
+  // Schedules placed out of order relative to start time.
+  const planActual =
+    new Plan({
+      sequences: [],
+      schedules: {
+        fall: [ lateSchedule, earlySchedule ],
+        winter: [],
+        summer: [],
+      },
+    });
+
+  // Schedules placed in order relative to start time.
+  const planExpected =
+    new Plan({
+      sequences: [],
+      schedules: {
+        fall: [ earlySchedule, lateSchedule ],
+        winter: [],
+        summer: [],
+      },
+    });
+
+  t.notDeepEqual(
+    planActual.schedules,
+    planExpected.schedules,
+    'Should not be equal since different order',
+  );
+
+  // Sort the out of order plan schedules.
+  planActual.sortSchedules({
+    preferences: createPreferencesFromEveningTimePreferences([ false, false, false ]),
   });
 
   t.deepEqual(
-    Plan.sortSchedules([ lateSchedule, earlySchedule ], termPreference), // Actual order
-    [ earlySchedule, lateSchedule ], // Expected order
+    planActual.schedules,
+    planExpected.schedules,
+    'Should be equal since now in same order',
   );
 });
+
+test('sortSchedules - eveningTimePreference - 3 schedules with 3 section each', (t) => {
+  const sections = [
+    createSectionFromStartTimes([ '20.05.00' ], 'FALL'),
+    createSectionFromStartTimes([ '08.45.00', '08.45.00' ], 'FALL'),
+    createSectionFromStartTimes([ '17.45.00', '20:15.00' ], 'FALL'),
+    createSectionFromStartTimes([ '10.45.00' ], 'FALL'),
+    createSectionFromStartTimes([ '11.45.00', '11.45.00' ], 'FALL'),
+    createSectionFromStartTimes([ '14.45.00' ], 'FALL'),
+  ];
+
+  const schedules = [
+    createScheduleFromSections(sections.slice(0, 3)), // Most late sections
+    createScheduleFromSections(sections.slice(3, 5)), // Mix of early and late
+    createScheduleFromSections([ sections[1], sections[3], sections[4] ]), // Most early sections
+  ];
+
+  // Schedules placed out of order relative to start time.
+  const planActual =
+    new Plan({
+      sequences: [],
+      schedules: {
+        fall: schedules,
+        winter: [],
+        summer: [],
+      },
+    });
+
+  // Schedules placed in order relative to start time.
+  const planExpected =
+    new Plan({
+      sequences: [],
+      schedules: {
+        fall: [ schedules[2], schedules[1], schedules[0] ],
+        winter: [],
+        summer: [],
+      },
+    });
+
+  t.notDeepEqual(
+    planActual.schedules,
+    planExpected.schedules,
+    'Should not be equal since different order',
+  );
+
+  // Sort the out of order plan schedules.
+  planActual.sortSchedules({
+    preferences: createPreferencesFromEveningTimePreferences([ false, false, false ]),
+  });
+
+  t.deepEqual(
+    planActual.schedules,
+    planExpected.schedules,
+    'Should be equal since now in same order',
+  );
+});
+
+function createPreferencesFromEveningTimePreferences(eveningTimePreferences) {
+  return new Preferences({
+    fall: new TermPreference({
+      term: 'FALL',
+      numberOfCourses: 5,
+      requestedCourses: [],
+      eveningTimePreference: eveningTimePreferences[0],
+      timeOfDay: 'MORNING', // Optional (based on eveningTimePreference)
+      offDays: [ 'FRIDAY' ],
+    }),
+    winter: new TermPreference({
+      term: 'WINTER',
+      numberOfCourses: 5,
+      requestedCourses: [],
+      eveningTimePreference: eveningTimePreferences[1],
+      timeOfDay: 'MORNING', // Optional (based on eveningTimePreference)
+      offDays: [ 'FRIDAY' ],
+    }),
+    summer: new TermPreference({
+      term: 'SUMMER',
+      numberOfCourses: 5,
+      requestedCourses: [],
+      eveningTimePreference: eveningTimePreferences[2],
+      timeOfDay: 'MORNING', // Optional (based on eveningTimePreference)
+      offDays: [ 'FRIDAY' ],
+    }),
+  });
+}
+
+function createSectionFromStartTimes(startTimes, term = 'FALL') {
+  const times = startTimes.length > 1 ?
+    [
+      new TimeBlock({
+        startTime: startTimes[0],
+        endTime: '10.15.00',
+        weekDay: 'MONDAY',
+      }),
+      new TimeBlock({
+        startTime: startTimes[1],
+        endTime: '10.15.00',
+        weekDay: 'MONDAY',
+      }),
+    ] :
+    [
+      new TimeBlock({
+        startTime: startTimes[0],
+        endTime: '10.15.00',
+        weekDay: 'MONDAY',
+      }),
+    ];
+
+  return new Section({
+    courseCode: 'COMP348',
+    code: 'H',
+    kind: 'LEC',
+    location: new Location({
+      code: 'H920',
+      building: 'Hall',
+      room: '920',
+    }),
+    mode: 'IN PERSON',
+    times,
+    dates: [
+      '01/05/2019',
+      '04/12/2019',
+    ],
+    term,
+  });
+}
+
+function createScheduleFromSections(sections) {
+  return new Schedule({
+    term: sections[0].term,
+    sections,
+  });
+}
